4af4140d17ffd2651e649a2ba604b7ea259c930a
Daniel Eni <enidaniel15@gmail.com>
2022-02-13 11:25:15 -1100

    make it better

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
 let catalogue = document.getElementById('catalogue')
 let home = document.getElementById('home')
+let about = document.getElementById('about')
+let aboutContent = document.querySelector('.about')
 let message = document.querySelector('.message')
 let glass = document.querySelector('.glass')
 let glassMobile = document.querySelector('.glassMobile')
@@ -12,6 +14,7 @@ let viewMobileImg = document.querySelectorAll('.mobileView img')
 catalogue.addEventListener('click', (e) => {
     e.preventDefault()
     home.style.display = 'block'
+    about.style.display = 'none'
     if (window.innerWidth <= '500') {
         message.classList.remove('animateReverse')
         glass.classList.remove('animateReverse')
@@ -51,13 +54,17 @@ catalogue.addEventListener('click', (e) => {
 home.addEventListener('click', (e) => {
     e.preventDefault()
     home.style.display = 'none'
+    about.style.display = 'block'
     if (window.innerWidth <= '500') {
         for (let i = 0; i < viewMobileImg.length; i++) {
             viewMobileImg[i].classList.remove('imageAnimate')
             viewMobileImg[i].classList.add('imageAnimateReverse')
         }
+        aboutContent.classList.add('aboutAnimateReverse')
+        aboutContent.classList.remove('aboutAnimate')
         setTimeout(() => {
             mobileView.style.display = 'none'
+            aboutContent.style.display = 'none'
             message.classList.remove('animate')
             message.classList.add('animateReverse')
             glass.classList.remove('animate')
@@ -72,8 +79,11 @@ home.addEventListener('click', (e) => {
             viewImg[i].classList.remove('imageAnimate')
             viewImg[i].classList.add('imageAnimateReverse')
         }
+        aboutContent.classList.add('aboutAnimateReverse')
+        aboutContent.classList.remove('aboutAnimate')
         setTimeout(() => {
             view.style.display = 'none'
+            aboutContent.style.display = 'none'
             message.classList.remove('animate')
             glass.classList.remove('animate')
             glassMobile.classList.remove('animate')
@@ -83,5 +93,50 @@ home.addEventListener('click', (e) => {
             glassMobile.classList.add('animateReverse')
             glassMobile2.classList.add('animateReverse')
         }, 2000)
+    } 
+})
+
+//For About Page
+about.addEventListener('click', (e) => {
+    e.preventDefault()
+    about.style.display = 'none'
+    home.style.display = 'block'
+    aboutContent.style.display = 'block'
+    if (window.innerWidth <= '500') {
+        aboutContent.classList.remove('aboutAnimateReverse')
+        aboutContent.classList.add('aboutAnimate')
+        message.classList.remove('animateReverse')
+        glass.classList.remove('animateReverse')
+        glassMobile.classList.remove('animateReverse')
+        glassMobile2.classList.remove('animateReverse')
+        message.classList.add('animate')
+        glass.classList.add('animate')
+        glassMobile.classList.add('animate')
+        glassMobile2.classList.add('animate')
+        // setTimeout(() => {
+        //     mobileView.style.display = 'flex'
+        //     for (let i = 0; i < viewMobileImg.length; i++) {
+        //         viewMobileImg[i].classList.remove('imageAnimateReverse')
+        //         viewMobileImg[i].classList.add('imageAnimate')
+        //     }
+        // }, 2000)
+    } else {
+        aboutContent.classList.remove('aboutAnimateReverse')
+        aboutContent.classList.add('aboutAnimate')
+        message.classList.remove('animateReverse')
+        glass.classList.remove('animateReverse')
+        glassMobile.classList.remove('animateReverse')
+        glassMobile2.classList.remove('animateReverse')
+        message.classList.add('animate')
+        glass.classList.add('animate')
+        glassMobile.classList.add('animate')
+        glassMobile2.classList.add('animate')
+        // setTimeout(() => {
+        //     view.style.display = 'flex'
+        //     for (let i = 0; i < viewImg.length; i++) {
+        //         viewImg[i].classList.remove('imageAnimateReverse')
+        //         viewImg[i].classList.add('imageAnimate')
+        //     }
+        // }, 2000)
     } 
 })
